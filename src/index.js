029626543852c137d943e63bd657f4f4e5c301eb
tu6619 <tu6619@my.bristol.ac.uk>
2016-08-16 23:05:47 +0100

    buttons added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Button, ButtonGroup } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 import ProgressBar from './components/progress_bar';
 
@@ -9,14 +9,23 @@ class App extends Component {
   render () {
 
     return (
-      <div className='grid'>
-        <Col xs={4} md={4}>
-        </Col>
-        <Col className='bar' xs={10} md={4}>
-          <ProgressBar />
-        </Col>
-        <Col xs={4} md={4}>
-        </Col>
+      <div>
+        <div className='grid'>
+          <Col xs={4} md={4}>
+          </Col>
+          <Col className='bar' xs={10} md={4}>
+            <ProgressBar />
+          </Col>
+          <Col xs={4} md={4}>
+          </Col>
+        </div>
+        <div className='button-group'>
+          <ButtonGroup justified>
+            <Button bsSize='large' href='#'>Left</Button>
+            <Button bsSize='large' href='#'>Middle</Button>
+            <Button bsSize='large' href="#">Middle</Button>
+          </ButtonGroup>
+        </div>
       </div>
     );
   }
