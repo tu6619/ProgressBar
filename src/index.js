16e055c801dc0030e834ebc3314fa5a2acfad58c
tu6619 <tu6619@my.bristol.ac.uk>
2016-08-16 22:35:02 +0100

    grid added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Col } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 import ProgressBar from './components/progress_bar';
 
@@ -8,8 +9,14 @@ class App extends Component {
   render () {
 
     return (
-      <div>
-        <ProgressBar />
+      <div className='grid'>
+        <Col xs={4} md={4}>
+        </Col>
+        <Col className='bar' xs={10} md={4}>
+          <ProgressBar />
+        </Col>
+        <Col xs={4} md={4}>
+        </Col>
       </div>
     );
   }
